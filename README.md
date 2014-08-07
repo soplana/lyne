d6cb2ba5b7876a6bdaf38115027f02ba21eb975d
soplana <sonosheet.jp@gmail.com>
2014-08-07 12:33:01 +0900

    update readme

--- README.md
+++ README.md
@@ -21,3 +21,21 @@ or
 ```
 gem 'lyne'
 ```
+
+optionで渡すcharacter_idはキャラクター選択画面の「このキャラを選択」ボタンのrel属性の値になります。
+
+![character_id](http://dl.dropboxusercontent.com/u/92653510/DQ10/charaid.png "character_id")
+
+```ruby
+require 'lyne'
+
+dqx = Lyne::Account.new(
+  account:      'account_name',
+  password:     'account_password',
+  character_id: 975829418411
+)
+
+info = dqx.character.info
+info.id   #=> "TV327-610"
+info.name #=> "ソプラナ"
+```
--- lib/lyne/page/show_table.rb
+++ lib/lyne/page/show_table.rb
@@ -1,12 +1,12 @@
-module Lyne::Page
-  module ShowTable
-    def show_table
-      table = Kosi::Table.new({header: table_headers})
-      print table.render([table_rows])
-    end
-
-    private
-    def table_headers; [] end
-    def table_rows;    [] end
-  end
-end
+#module Lyne::Page
+#  module ShowTable
+#    def show_table
+#      table = Kosi::Table.new({header: table_headers})
+#      print table.render([table_rows])
+#    end
+#
+#    private
+#    def table_headers; [] end
+#    def table_rows;    [] end
+#  end
+#end
